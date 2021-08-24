a94154184a5785ec4a90b52096d902617b131783
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-24 00:42:28 -0300

    Override: Function - onBindViewHolder (presenter.adapter.LiveItemAdapter)

--- app/src/main/java/com/study/kotlin/aliveinlive/presenter/adapter/LiveItemAdapter.kt
+++ app/src/main/java/com/study/kotlin/aliveinlive/presenter/adapter/LiveItemAdapter.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.study.kotlin.aliveinlive.R
 import com.study.kotlin.aliveinlive.domain.model.Live
+import kotlinx.android.synthetic.main.live_item.view.*
 
 class LiveItemAdapte(
     private val context: Context,
@@ -21,6 +24,27 @@ class LiveItemAdapte(
     }
 
     override fun onBindViewHolder(holder: LiveItemViewHodler, position: Int) {
+
+        val live = liveList[position]
+
+        with(holder.itemView){
+
+            txtLiveTitle.text = live.title
+            txtLiveAuthor.text = live.author
+
+            val requestOptions = RequestOptions()
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .error(R.drawable.ic_launcher_background)
+
+            Glide.with(context)
+                .applyDefaultRequestOptions(requestOptions)
+                .load(live.thumbnailURL)
+                .into(imgLiveThumbnail)
+
+            this.setOnClickListener {
+                onItemClicked(live)
+            }
+        }
 
     }
 
