2748fd8ef9eed361d8d59f2567f7227e22e06aa7
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-24 00:19:32 -0300

    Create: ViewHolder - Main (presenter.adapter.LiveItemAdapter)

--- app/src/main/java/com/study/kotlin/aliveinlive/presenter/adapter/LiveItemAdapter.kt
+++ app/src/main/java/com/study/kotlin/aliveinlive/presenter/adapter/LiveItemAdapter.kt
@@ -1,4 +1,9 @@
 package com.study.kotlin.aliveinlive.presenter.adapter
 
-class LiveItemAdapter {
-}
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.study.kotlin.aliveinlive.domain.model.Live
+
+
+
+class LiveItemHodler (itemView: View): RecyclerView.ViewHolder(itemView)
