c96599999152059fda292797f2f1c3a17215f32a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-24 00:29:37 -0300

    Override: Function - onCreateViewHolder (presenter.adapter.LiveItemAdapter)

--- app/src/main/java/com/study/kotlin/aliveinlive/presenter/adapter/LiveItemAdapter.kt
+++ app/src/main/java/com/study/kotlin/aliveinlive/presenter/adapter/LiveItemAdapter.kt
@@ -1,9 +1,32 @@
 package com.study.kotlin.aliveinlive.presenter.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.study.kotlin.aliveinlive.R
 import com.study.kotlin.aliveinlive.domain.model.Live
 
+class LiveItemAdapte(
+    private val context: Context,
+    private val live:Live,
+    private val onItemClicked: (Live) -> Unit
+): RecyclerView.Adapter<LiveItemViewHodler>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LiveItemViewHodler {
 
-class LiveItemHodler (itemView: View): RecyclerView.ViewHolder(itemView)
+        val view = LayoutInflater.from(context).inflate(R.layout.live_item, parent, false)
+        return LiveItemViewHodler(view);
+    }
+
+    override fun onBindViewHolder(holder: LiveItemViewHodler, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class LiveItemViewHodler (itemView: View): RecyclerView.ViewHolder(itemView)
