c727025a263126d0b1bcf033914686d1f5dabb25
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-24 08:25:55 -0300

    Create: Function - Setup (presenter.main.MainActivity)

--- app/src/main/java/com/study/kotlin/aliveinlive/presenter/main/MainActivity.kt
+++ app/src/main/java/com/study/kotlin/aliveinlive/presenter/main/MainActivity.kt
@@ -3,11 +3,16 @@ package com.study.kotlin.aliveinlive.presenter.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputBinding
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.study.kotlin.aliveinlive.R
 import com.study.kotlin.aliveinlive.data.api.RetrofitService
+import com.study.kotlin.aliveinlive.data.repositories.MainRepository
 import com.study.kotlin.aliveinlive.interfaces.API
 import com.study.kotlin.aliveinlive.presenter.adapter.LiveItemAdapter
 import com.study.kotlin.aliveinlive.presenter.main.viewmodel.MainViewModel
+import com.study.kotlin.aliveinlive.presenter.main.viewmodel.MainViewModelFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,5 +25,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setup()
+    }
+
+    fun setup(){
+        setupViewModel()
+        setupRecyvlerView()
+    }
+
+
+
+    fun setupRecyvlerView(){
+        rcvLives.layoutManager = LinearLayoutManager(this)
     }
 }
